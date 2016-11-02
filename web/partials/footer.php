60e1826ed558ca5f0a68b2056e09aad7cfdc0235
Nils Langner <contact@phmlabs.com>
2016-11-02 10:44:20 +0100

    added tos

--- web/partials/footer.php
+++ web/partials/footer.php
@@ -1,13 +1,17 @@
 <footer>
     <div class="content">
         <!--img src="/images/logo.png" width="80px"-->
-        <div style="width: 100%; text-align: center;">
-            Imprint:
-            <strong>Koalamon GbR</strong> |
-            Nils Langner |
-            Quellenweg 4 |
-            20535 Hamburg |
-            Germany
+        <div style="width: 100%; height: 10px">
+            <div style="float: left">Imprint:
+                <strong>Koalamon GbR</strong> |
+                Nils Langner |
+                Quellenweg 4 |
+                20535 Hamburg |
+                Germany
+            </div>
+            <div style="float: right">
+                <a href="/tos.html" style="color: white">Terms of Service</a>
+            </div>
         </div>
     </div>
 </footer>
