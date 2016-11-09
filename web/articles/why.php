85539450334cba536d5d9e0f295f632865047f70
Nils Langner <contact@phmlabs.com>
2016-11-09 07:45:26 +0100

    boostrap; why leankoala?

---
+++ web/articles/why.php
@@ -0,0 +1,72 @@
+<div id="why">
+    <div class="content">
+        <h1 style="width: 100%; text-align: center; margin-bottom: 100px">Why Leankoala?</h1>
+    </div>
+    <div class="content" style="width: 1250px;">
+
+        <ul>
+            <li>
+                <h3>Perfect for agencies</h3>
+                <p>
+                    On the one hand customers want quality but on the other customers don't want to pay for it. With Leankoala we
+                    provide a strategy and the suitable test suite that focuses on efficient and affordable quality assurance.
+                </p>
+                <a href="#agency">read more ...</a>
+            </li>
+            <li>
+                <h3>Great for publishing companies</h3>
+                <p>
+                    The idea behind lean testing and Leankoala originated from eight years testing websites from two of the biggest publishing
+                    companies in europe. We know how to handle editorial content that meets technology.
+                </p>
+            </li>
+            <li>
+                <h3>Easy for website operators</h3>
+                <p>
+                    When developing Leankoala one thing we always had in mind was that it must be easy to solve standard problems and possible to
+                    solve the hard ones. For all website operators we kept the entry threshold low.
+                </p>
+            </li>
+            <li>
+                <h3>Efficiency for quality managers</h3>
+                <p>
+                    Hitting your website with heavy tools can be very expensive. Having a risk based lean approach reduces cost and helps
+                    testing your websites much faster with less effort.
+                </p>
+            </li>
+        </ul>
+
+    </div>
+
+    <div class="content" id="why-details">
+        <div style="clear: both"></div>
+
+        <a name="agency">
+            <h2>Perfect for agencies</h2>
+        </a>
+
+        <div style="float: left; margin-right: 30px">
+            <i class="fa fa-cubes" aria-hidden="true"></i>
+        </div>
+
+        <p>
+            The agency business can be very tough. Customers want their web projects to be developed in no time, with low costs and high
+            quality. That to achieve is almost impossible. In many cases one of the three goals has to be dropped and mostly it's quality.
+        </p>
+        <p>
+            Our observations revealed dropping quality does not equal that it is the least important goal. In our opinion it is often a wrong
+            understanding of what quality means. Most projects are from an objective point of view <span style="">only</span> standard. You connect standard features
+            and components to create a new system. Additionally there are only a few use cases in that system that have real business values.
+        </p>
+        <p>
+            For testing those <strong>standard features</strong> Leankoala provides <strong>standard checks</strong>. Often that is all that
+            is needed. In addition we offer a new and different testing concept we already verified on many conferences, press article and the
+            german book
+            "<a href="https://www.amazon.de/Erfolgreiche-Softwareprojekte-Web-Gedanken-Webentwicklung/dp/3868020888" target="_blank">Erfolgreiche
+                Softwareprojekt im Web</a>". One of our goals is to push the lean testing movement so you don't have to sell it to your client.
+        </p>
+
+        <div style="clear: both"></div>
+    </div>
+
+</div>
