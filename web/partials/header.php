d08e211b047221c213a8c0441989f77210afe04e
Nils Langner <contact@phmlabs.com>
2016-11-21 14:49:40 +0100

    mobile

--- web/partials/header.php
+++ web/partials/header.php
@@ -1,33 +1,32 @@
 <div class="content">
     <header>
-        <div class="logo">
-            <a href="/"><img src="/images/logo_small.png"></a>
-        </div>
-        <div class="menu">
-            <ul class="nav nav-pills">
-                <li>
-                    <a href="/why.html">Why Leankoala?</a>
-                </li>
-                <li>
-                    <a href="/features.html">Product</a>
-                </li>
-                <!--li>
-                    Customer
-                </li-->
-                <li>
-                    <a href="/pricing.html">Pricing</a>
-                </li>
-                <li>
-                    <a href="https://blog.leankoala.com/">Blog</a>
-                </li>
-                <li>
-                    <a href="https://monitor.leankoala.com/secure_area/login">Login</a>
-                </li>
-                <li class="signup">
-                    <a href="https://monitor.leankoala.com/secure_area/register/">Sign Up</a>
-                </li>
-            </ul>
-        </div>
-        <div style="clear: both"></div>
+
+
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="logo">
+                <a href="/">
+                    <img src="/images/logo_small.png">
+                </a>
+            </div>
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <i class="fa fa-bars" aria-hidden="true" style="color: #999;"></i>
+                    </button>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/why.html">Why Leankoala?</a></li>
+                        <li><a href="/features.html">Product</a></li>
+                        <li><a href="/pricing.html">Pricing</a></li>
+                        <li><a href="https://blog.leankoala.com/">Blog</a></li>
+                        <li><a href="https://monitor.leankoala.com/secure_area/register/"><strong>Sign Up</strong></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
     </header>
 </div>
