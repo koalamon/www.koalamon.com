f50c01efa6285d42739fa49b76efc6db4713c3dd
Nils Langner <contact@phmlabs.com>
2016-11-25 12:46:57 +0100

    on the road

---
+++ web/articles/on-the-road.php
@@ -0,0 +1,131 @@
+<div class="content">
+    <h1 style="width: 100%; text-align: center">On the road</h1>
+</div>
+
+<div class="content">
+    <div class="main-info">
+        <p>
+            As we, the members of the Leankoala team, are very proud of our software and the lean testing approach we try to talk a lot about it. These are
+            our talks so far. As we are a Germany based company most of the slides are in german at the moment.
+        </p>
+    </div>
+
+    <style>
+
+        .sessions {
+        }
+
+        .sessions li {
+            margin-top: 120px;
+            border-top: 1px solid #eee;
+            padding-top: 80px;
+        }
+
+        .sessions li:first-child {
+            margin-top: 50px;
+            border-top: none;
+        }
+
+        .sessions .session-image img {
+            width: 400px;
+            border: 1px solid #eee;
+        }
+
+        .sessions .session-image {
+            float: left;
+            margin-right: 30px;
+        }
+
+        .sessions .event {
+            font-size: 12px;
+        }
+
+        .sessions .topic {
+            margin-top: 10px;
+            font-size: 20px;
+            font-weight: 400;
+        }
+
+        .sessions .description {
+            margin-top: 20px;
+            line-height: 30px;
+            font-size: 13px;
+            padding-left: 432px;
+        }
+
+        .sessions .recommendations {
+            margin-top: 20px;
+        }
+
+        .sessions .recommendations li {
+            margin-top: 0;
+            padding: 2px 5px;
+            font-size: 12px;
+            float: left;
+            border-radius: 5px;
+            background-color: #eee;
+            margin-right: 10px;
+            margin-top: 10px;
+        }
+    </style>
+
+    <ul class="sessions">
+        <li>
+            <div class="session-image">
+                <img src="/images/road/webmontag2016.png">
+            </div>
+            <div class="event">2016-11-28 - Webmontag #40</div>
+            <div class=" topic">
+                The lean tester - und seine Geschichte der Qualität (German)
+            </div>
+            <div class="description">
+                <p>Wie hat sich das Testen von Software in der letzten Zeit verändert und ist das Web vielleicht gar nicht der richtige Ort für Qualität?</p>
+                <p>
+                    Momentan steht die Webentwicklung unter dem Stern der durchdachten Architekturen, des sauberen Codes und der hohen Testabdeckung. Viele Projekte sterben dadurch leider in
+                    Schönheit. Wie man damit im Bereich Testing umgehen kann und warum es sinnvoll sein kann umzudenken, soll dieser Vortrag anreißen.
+                </p>
+
+                <ul class="recommendations">
+                    <li><a href="http://www.webmontag-hamburg.de/">Webmontag Hamburg</a></li>
+                    <li><a href="http://www.webmontag-hamburg.de/">Demo: Webmontag Hamburg</a></li>
+                    <li><a href="https://monitor.leankoala.com/p/demo-sah/">Demo: Software Allianz Hamburg</a></li>
+                    <li><a href="http://www.webmontag-hamburg.de/">Demo: Techniker Krankenkasse</a></li>
+                </ul>
+
+            </div>
+        </li>
+        <li>
+            <div class="session-image">
+                <a href="http://www.slideshare.net/phphatesme/aller-guten-dinge-sind-drei-code-talks-2016">
+                    <img src="/images/road/codetalks.png">
+                </a>
+            </div>
+            <div class="event">2016-09-29 - CodeTalks 2016</div>
+            <div class=" topic">
+                Aller guten Dinge sind drei (German)
+            </div>
+            <div class="description">
+                <p>
+                    Was man hätte lernen können, wenn man drei Mal Qualitätsmanagement in unterschiedlichen Unternehmen aufgebaut hat. Nils und Torsten haben das Qualitätsmanagement im Digitalbereich
+                    bei Europas größten Verlagshaus etabliert. Dann trennten sich ihre Wege, damit jeder für sich in einer anderen Firma sich noch mal dem Thema Qualität widmen kann.
+                </p>
+                <p>
+                    Zusammen blicken
+                    die beiden auf ihre gemeinsame “dunkle” Vergangenheit zurück, erzählen was alles schiefgehen kann und präsentieren ihre neuen Vorgehensweisen in ihren jetzigen Unternehmen. Danach
+                    analysieren sie warum das absolute Chaos ausbrechen würde, wenn man die Methodik des jeweiligen anderen auf das eigene Unternehmen anwendet.
+                </p>
+                <p>
+                    Der Talk befasst sich auf humorvolle Art mit Themen wie Testautomatisierung, Werkzeuge, Business Value und schlanken Vorgehensweisen (Lean Testing).
+                </p>
+
+                <ul class="recommendations">
+                    <li><a href="https://www.codetalks.de/">Code.Talks</a></li>
+                    <li><a href="http://www.about-you.de/">About You</a></li>
+                    <li><a href="http://www.bauerxcel.de/">Bauer Xcel</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<div style="clear: both"></div>
