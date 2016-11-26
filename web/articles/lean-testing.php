8d46efe5887a659c282cfe453a0358772b7b606a
Nils Langner <contact@phmlabs.com>
2016-11-26 09:25:26 +0100

    lean testing

---
+++ web/articles/lean-testing.php
@@ -0,0 +1,53 @@
+<div id="lean">
+    <div class="content">
+        <h1 style="width: 100%; text-align: center; margin-bottom: 20px">What is lean testing?</h1>
+        <div style="width: 100%; text-align: center; margin-bottom: 100px">
+            <img src="/images/lean.png">
+        </div>
+    </div>
+
+    <style>
+        .info {
+            font-size: 30px;
+            font-weight: 100;
+            text-align: center;
+        }
+
+        #lean p {
+            line-height: 30px;
+        }
+    </style>
+
+    <div class="content info">
+        Lean testing for web applications means testing for symptoms instead of root causes.
+    </div>
+
+    <div class="content">
+
+        <h2>Testing for symptoms not root causes.</h2>
+
+        <img src="/images/lean/symptoms.png" width="600px" style="float: right; margin-left: 30px; margin-bottom: 30px">
+
+        <p>
+            The term lean testing for web applications was established in 2013 by Nils Langner, one of our founders. It was the result of
+            the observation that there are some simple tests or checks for websites that always fail when a website or a feature is broken.
+        </p>
+        <p>
+            At that time it was normal that test coverage was very high. Complex functional tests with selenium where on the agenda. Behavior
+            Driven Development was on its high point. It was the time of great architecture and clean code. The only problem with the
+            clean approach was the time it took implement and test an application.
+        </p>
+        <p>
+
+        </p>
+        <p>
+            When using the lean testing approach and finding the right simple checks there is a good chance to create a good enough test coverage
+            within a fraction of the time and costs.
+        </p>
+        <!--p>
+            From a project management point of view Eric Ries fought that development with his Lean Startup approach. It helped focussing
+            on a minimal viable product and shipping your application early. But
+        </p-->
+
+    </div>
+</div>
