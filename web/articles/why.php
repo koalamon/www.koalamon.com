512c4515976a6a55af018272fba449f211b2d0b5
Nils Langner <contact@phmlabs.com>
2017-02-08 09:50:39 +0100

    why texts

--- web/articles/why.php
+++ web/articles/why.php
@@ -8,18 +8,6 @@
     <div class="content" id="why-block-personas">
         <ul>
             <li>
-                <h3><?php __('page.why.overview.agency.headline'); ?></h3>
-                <p><?php __('page.why.overview.agency.text'); ?></p>
-                <a href="#agency" class="readmore"><?php __('page.why.readmore'); ?></a>
-            </li>
-            <li>
-                <h3><?php __('page.why.overview.publishers.headline'); ?></h3>
-                <p>
-                    <?php __('page.why.overview.publishers.text'); ?>
-                </p>
-                <a href="#publishing" class="readmore"><?php __('page.why.readmore'); ?></a>
-            </li>
-            <li>
                 <h3><?php __('page.why.overview.operators.headline'); ?></h3>
                 <p>
                     <?php __('page.why.overview.operators.text'); ?>
@@ -32,6 +20,18 @@
                     <?php __('page.why.overview.quality.text'); ?>
                 </p>
             </li>
+            <li>
+                <h3><?php __('page.why.overview.agency.headline'); ?></h3>
+                <p><?php __('page.why.overview.agency.text'); ?></p>
+                <a href="#agency" class="readmore"><?php __('page.why.readmore'); ?></a>
+            </li>
+            <li>
+                <h3><?php __('page.why.overview.publishers.headline'); ?></h3>
+                <p>
+                    <?php __('page.why.overview.publishers.text'); ?>
+                </p>
+                <a href="#publishing" class="readmore"><?php __('page.why.readmore'); ?></a>
+            </li>
         </ul>
 
     </div>
@@ -40,30 +40,14 @@
         <div style="clear: both"></div>
 
         <a name="agency">
-            <h2>Perfect for agencies</h2>
+            <h2><?php __('page.why.overview.agency.headline'); ?></h2>
         </a>
 
         <div style="float: left; margin-right: 30px">
             <i class="fa fa-cubes" aria-hidden="true"></i>
         </div>
 
-        <p>
-            The agency business can be very tough, as customers want their web projects to be developed in no time, with low costs and high
-            quality. It is almost impossible to achieve this, so one of the three goals has to be dropped. And most often it is quality that suffers in the end.
-        </p>
-        <p>
-            Our experience shows that the fact quality is most often dropped does not mean it is the least important goal. This misunderstaning comes from the definition of quality as such.
-            understanding of what quality means. Most projects are from an objective point of view <span style="">only</span> standard. You connect standard features
-            To shed some lights on this: most projects are pretty much standard in the way they are setup, where you connect standard features and components to create a new system.
-            Additionally, there will be only a few cases in this system that add real business values.
-        </p>
-        <p>
-            Leankoala provides <strong>standard checks</strong> for testing those <strong>standard features</strong>. Often that is all that
-            is needed. In addition, we offer a new and different testing concept we already verified in many conferences, press articles and the
-            German book
-            "<a href="https://www.amazon.de/Erfolgreiche-Softwareprojekte-Web-Gedanken-Webentwicklung/dp/3868020888" target="_blank">Erfolgreiche
-                Softwareprojekt im Web</a>". One of our goals is to push the lean testing movement so you don't have to sell it to your client.
-        </p>
+        <?php __('page.why.overview.agency.full'); ?>
 
         <div class="partner" style="margin-left: 140px; margin-top: 50px">
             <ul>
@@ -84,55 +68,29 @@
     <div class="main-element-grey">
         <div class="content" id="why-details">
             <a name="publishing">
-                <h2>Great for publishing companies</h2>
+                <h2><?php __('page.why.overview.publishers.headline'); ?></h2>
             </a>
 
             <div style="float: left; margin-right: 30px">
                 <i class="fa fa-newspaper-o" aria-hidden="true"></i>
             </div>
 
-            <p>
-                When creating Leankoala, we always had one target group in mind: publishing companies. This is not only because of our background in working for two publishing companies which are
-                among the largest in Europe
-                but also because the Lean Testing approach perfectly matches requirements of editorial websites.
-            </p>
-
-            <p>
-                Having a look at websites like www.stern.de or www.cosmopolitan.de with hundreds of thousands of articles, testing them seems quite complex. Having a closer look at those pages shows
-                that the complexity is not that high. In most casas, there are only a few components like: standard article, tag page, search results or category page. That is almost everything to
-                you need to test. Additionally, there are pages like the sitemap or smaller APIs used with almost no user interaction with the page.
-            </p>
-
-            <p>
-                We learned that when trying to test such a website, there are only a few key tests you need to write. For example, a standard article can be tested
-                with four checks: is the headline existing, is the page footer existing, is google analytics pixel counted and do javascript
-                errors occur. If none of these checks fail, the chance that the website is working properly is as high as 95 percent. <strong>With Leankoala such checks are
-                    created within minutes.</strong>
-            </p>
+            <?php __('page.why.overview.publishers.full'); ?>
 
             <div style="clear: both"></div>
         </div>
     </div>
     <div class="content" id="why-details">
         <a name="operators">
-            <h2>Easy for website operators</h2>
+            <h2><?php __('page.why.overview.operators.headline'); ?></h2>
         </a>
 
         <div style="float: left; margin-right: 30px; margin-bottom: 50px">
             <i class="fa fa-user-circle" aria-hidden="true""></i>
         </div>
 
-        <p>
-            There are a lot of businesses that depend on their websites working properly. If the sites do not work the revenue will drop as well. The challenge such businesses face
-            is that they often can't afford a quality assurance team.
-        </p>
+        <?php __('page.why.overview.operators.full'); ?>
 
-        <p>
-            With Leankoala we provide software as a service solution that helps writing easy system tests with just a few clicks. Using our wizards
-            everybody is able to build up a good testing foundation for any website. We achieve this by focusing on modern websites where we know
-            what the most common features are. For those features, we provide predefined tests on top of our tools. If it is necessary to go deeper,
-            all of our tools can also be manually configured.
-        </p>
         <div class="one-day">
             <a href="<?php __a('/one-day-with/amilio.html') ?>" class="more">one day with amilio</a>
         </div>
