5c62d17b1a8115486eec4200ee64502a63167eb2
phmLabs <nils.langner@phmlabs.com>
2017-02-12 06:39:27 +0100

    product/tool, one-day-with: de

--- web/articles/one-day-with.php
+++ web/articles/one-day-with.php
@@ -1,10 +1,8 @@
 <div class="content" style="margin-bottom: 100px">
-    <h1 style="width: 100%; text-align: center">One day with</h1>
+    <h1 style="width: 100%; text-align: center"><?php __("page.one-day-with.headline"); ?></h1>
 
     <div class="main-info">
-        Testing many websites and working for many companies means we have a lot of stories to tell. And so we thought of introducing
-        our "one day with" section, where all our readers can find inspiration. We want to tell interesting stories
-        about what we achieved within one day of working together with some of our clients.
+        <?php __("page.one-day-with.info"); ?>
     </div>
 
     <style>
@@ -39,17 +37,21 @@
     <table style="margin-top: 100px;">
         <tr style="border-bottom: 100px solid transparent" class="plan-medium">
             <td style="width: 250px; vertical-align: top">
-                <img src="/images/startups/amilio2.png" style="position: relative; top: 7px"><img src="/images/startups/amilio1.png">
+                <img src="/images/startups/amilio2.png" style="position: relative; top: 7px"><img
+                        src="/images/startups/amilio1.png">
             </td>
             <td>
                 <div style="font-weight: 600; margin-bottom: 20px">amilio - Social. Shopping. Community.</div>
                 <div style="line-height: 28px; margin-right: 50px; font-size: 14px">
-                    amilio is a shopping community based on affiliate programs. Every user is able create lists of products
+                    amilio is a shopping community based on affiliate programs. Every user is able create lists of
+                    products
                     he or she likes. All those lists and products can be liked and shared by others.
                 </div>
             </td>
             <td width="150px">
-                <a href="<?php __a('/one-day-with/amilio.html'); ?>" style="padding: 10px; background-color: #27ae60; border-radius: 5px; color: white; font-size: 12px">read one day with amilio</a>
+                <a href="<?php __a('/one-day-with/amilio.html'); ?>"
+                   style="padding: 10px; background-color: #27ae60; border-radius: 5px; color: white; font-size: 12px">read
+                    one day with amilio</a>
             </td>
         </tr>
         <tr class="plan-small" style="border-bottom: 100px solid transparent">
@@ -63,7 +65,9 @@
                 </div>
             </td>
             <td width="150px">
-                <a href="<?php __a('/one-day-with/thewebhatesme.html'); ?>'" style="padding: 10px; background-color: #27ae60; border-radius: 5px; color: white; font-size: 12px">read one day with twhm</a>
+                <a href="<?php __a('/one-day-with/thewebhatesme.html'); ?>'"
+                   style="padding: 10px; background-color: #27ae60; border-radius: 5px; color: white; font-size: 12px">read
+                    one day with twhm</a>
             </td>
         </tr>
 
@@ -74,12 +78,14 @@
             <td>
                 <div style="font-weight: 600; margin-bottom: 20px">Scalamento</div>
                 <div style="line-height: 28px; margin-right: 50px; font-size: 14px">
-                    The Scalamento team helps companies to scale their momentum. They are experts for digital transformation and
+                    The Scalamento team helps companies to scale their momentum. They are experts for digital
+                    transformation and
                     agile leadership.
                 </div>
             </td>
             <td width="150px">
-                <a style="padding: 10px; background-color: #95a5a6; border-radius: 5px; color: white; font-size: 12px">coming soon</a>
+                <a style="padding: 10px; background-color: #95a5a6; border-radius: 5px; color: white; font-size: 12px">coming
+                    soon</a>
             </td>
         </tr>
 
