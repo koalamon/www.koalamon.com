8483c633120fad507e8f2b752b92ea72209fdf91
phmLabs <nils.langner@phmlabs.com>
2017-02-19 20:04:53 +0100

    steffies texte; recolution

--- web/articles/pricing.php
+++ web/articles/pricing.php
@@ -31,40 +31,44 @@
 
 <div class="content" style="text-align: center; margin-top: 30px; padding-bottom: 20px; margin-bottom: 30px">
     <div id="index-customers">
-        <div style="font-weight: 400; padding: 20px; margin-bottom: 20px;>
-            <a name=" startup
-        " style="padding-top: 20px">Hamburg Start Ups</a>
+        <div style="font-weight: 400; padding: 20px; margin-bottom: 20px;">
+            <a name="startup" style="padding-top: 20px">
+                Hamburg Start Ups
+            </a>
+        </div>
+        <ul style="" class="startups">
+            <li>
+                <a href="http://hacker-school.de/"><img src="/images/startups/hackerschool.png" style="margin-top: 13px"></a>
+            </li>
+            <li>
+                <a href="http://www.recolution.de/"><img src="/images/startups/recolution.png" style="margin-top: 16px; width: 210px"></a>
+            </li>
+            <li>
+                <a href="http://www.wetimebox.com/"><img src="/images/startups/wetimebox.png" style="margin-top: 15px"></a>
+            </li>
+            <li>
+                <a href="http://yuscale.com/"><img src="/images/startups/yuscale.png" style="margin-top: 15px; height: 45px;"></a>
+            </li>
+            <li>
+                <a href="https://www.thewebhatesme.com"><img src="/images/startups/whm.png"></a>
+            </li>
+            <li>
+                <a href="http://www.amilio.de"><img src="/images/startups/amilio2.png" style="position: relative; top: 7px"><img src="/images/startups/amilio1.png"></a>
+            </li>
+            <li>
+                <a href="http://buzzrank.de/"><img src="/images/startups/buzzrank.png" style="margin-top: 13px"></a>
+            </li>
+        </ul>
+        <div style="clear: both"></div>
     </div>
-    <ul style="" class="startups">
-        <li>
-            <a href="http://hacker-school.de/"><img src="/images/startups/hackerschool.png" style="margin-top: 13px"></a>
-        </li>
-        <li>
-            <a href="http://www.wetimebox.com/"><img src="/images/startups/wetimebox.png" style="margin-top: 15px"></a>
-        </li>
-        <li>
-            <a href="http://yuscale.com/"><img src="/images/startups/yuscale.png" style="margin-top: 15px; height: 45px;"></a>
-        </li>
-        <li>
-            <a href="https://www.thewebhatesme.com"><img src="/images/startups/whm.png"></a>
-        </li>
-        <li>
-            <a href="http://www.amilio.de"><img src="/images/startups/amilio2.png" style="position: relative; top: 7px"><img src="/images/startups/amilio1.png"></a>
-        </li>
-        <li>
-            <a href="http://buzzrank.de/"><img src="/images/startups/buzzrank.png" style="margin-top: 13px"></a>
-        </li>
-    </ul>
-    <div style="clear: both"></div>
-</div>
 </div>
 
 <div style="clear: both"></div>
 
 <div style="margin-top: 50px">
     <a href="mailto:nils.langner@koalamon.com?subject=Moin%20Moin%20Leankoala" style="color: white;">
-        <div class="register" style="background-color: #E30613; width: 120px; margin-left: calc(50% - 80px)">
-            <img src="/images/hamburg.png" width="20px" style="float: left; margin-right: 20px; margin-top: -5px"> contact us
+        <div class="register" style="background-color: #E30613; width: 160px; margin-left: calc(50% - 80px)">
+            <img src="/images/hamburg.png" width="20px" style="float: left; margin-right: 20px; margin-top: -5px"> <?php __('page.pricing.startup.contact'); ?>
         </div>
     </a>
 </div>
