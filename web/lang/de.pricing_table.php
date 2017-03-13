dec6f3f4b0bf39e9cfc01163cdd9795dbe2a5e91
Steffie <frey.stefanie@gmail.com>
2017-03-13 13:06:14 +0100

    Page, Project, Company

--- web/lang/de.pricing_table.php
+++ web/lang/de.pricing_table.php
@@ -2,26 +2,12 @@
     <table style="margin: 20px; margin-top: 50px">
         <tr style="border-bottom: 20px solid transparent">
             <td style="vertical-align: top; border-right: 20px solid transparent">
-                <strong>Small</strong>
+                <strong>Web Page</strong>
             </td>
             <td style="border-right: 40px solid transparent">
-                5 Komponenten<br>
+                20 Komponenten<br>
                 Ping-Interval 5 Minuten<br>
-                Unendlich Alarmierungen<br>
-                E-Mail-Support
-            </td>
-            <td style="vertical-align: top;">
-                9 EUR
-            </td>
-        </tr>
-        <tr style="border-bottom: 20px solid transparent">
-            <td style="vertical-align: top; border-right: 20px solid transparent">
-                <strong>Medium</strong>
-            </td>
-            <td style="border-right: 40px solid transparent">
-                25 components<br>
-                Ping interval 5 minutes<br>
-                Unlimited alerts<br>
+                Unbegrenzte Alarmierungen<br>
                 Live Support
             </td>
             <td style="vertical-align: top;">
@@ -30,7 +16,7 @@
         </tr>
         <tr style="border-bottom: 20px solid transparent">
             <td style="vertical-align: top; border-right: 20px solid transparent">
-                <strong>Large</strong>
+                <strong>Web Project</strong>
             </td>
             <td style="border-right: 40px solid transparent">
                 50 components<br>
@@ -40,6 +26,20 @@
             </td>
             <td style="vertical-align: top;">
                 69 EUR
+            </td>
+        </tr>
+        <tr style="border-bottom: 20px solid transparent">
+            <td style="vertical-align: top; border-right: 20px solid transparent">
+                <strong>Web Company</strong>
+            </td>
+            <td style="border-right: 40px solid transparent">
+                Unlimited components<br>
+                Ping interval 5 minutes<br>
+                Unlimited alerts<br>
+                Live Support
+            </td>
+            <td style="vertical-align: top;">
+                299 EUR
             </td>
         </tr>
     </table>
@@ -61,15 +61,15 @@
             </a>
         </li-->
         <li>
-            <h3>Small</h3>
-            <div class="feature"><strong>5</strong> Komponenten</div>
+            <h3>Web Page</h3>
+            <div class="feature"><strong>20</strong> Komponenten</div>
             <div class="feature">Ping-Interval <strong>5 Minuten</strong></div>
             <div class="feature">Stündliche Überprüfungen</div>
             <div class="feature">-</div>
             <div class="feature">-</div>
             <div class="feature">Keine Alarmierungsgrenzen</div>
-            <div class="feature">E-Mail-Support</div>
-            <div class="price">9 EUR</div>
+            <div class="feature">Live-Support</div>
+            <div class="price">39 EUR</div>
             <div class="info">pro Monat + 19% MwSt.</div>
             <div class="for-whom">Für kleine Webseiten wie z.B. Wordpress-Blogs.</div>
             <a href="https://monitor.leankoala.com/secure_area/register/">
@@ -79,15 +79,15 @@
         <li class="active">
             <div class="recommended">am beliebtesten</div>
             <div></div>
-            <h3>Medium</h3>
-            <div class="feature"><strong>20</strong> Komponenten</div>
+            <h3>Web Projekt</h3>
+            <div class="feature"><strong>50</strong> Komponenten</div>
             <div class="feature">Ping-Interval <strong>5 Minuten</strong></div>
             <div class="feature">Stündliche Überprüfungen</div>
             <div class="feature">Manuelles Starten der Tests</div>
             <div class="feature">-</div>
             <div class="feature">Keine Alarmierungsgrenzen</div>
             <div class="feature">Live Support</div>
-            <div class="price">39 EUR</div>
+            <div class="price">69 EUR</div>
             <div class="info">pro Monat + 19% MwSt.</div>
             <div class="for-whom">Für mittelgroße Webseite, wie Firmenauftritte.</div>
             <a href="https://monitor.leankoala.com/secure_area/register/">
@@ -95,15 +95,15 @@
             </a>
         </li>
         <li>
-            <h3>Large</h3>
-            <div class="feature"><strong>50</strong> Komponenten</div>
+            <h3>Web Company</h3>
+            <div class="feature"><strong>Unbegrenzte</strong> Komponenten</div>
             <div class="feature">Ping-Interval <strong>5 Minuten</strong></div>
             <div class="feature">Stündliche Überprüfungen</div>
             <div class="feature">Manuelles Starten der Tests</div>
             <div class="feature">Automatisches Starten der Tests</div>
             <div class="feature">Keine Alarmierungsgrenzen</div>
             <div class="feature">Live Support</div>
-            <div class="price">69 EUR</div>
+            <div class="price">299 EUR</div>
             <div class="info">pro Monat + 19% MwSt.</div>
             <div class="for-whom">Für große Webapplikationen mit vielen Funktionalitäten.</div>
             <a href="https://monitor.leankoala.com/secure_area/register/">
