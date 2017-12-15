70199efaba11ec58b4643629985ad4348d708f21
Nils Langner <nils.langner@leankoala.com>
2017-12-15 21:16:14 +0100

    media texte

--- web/articles/media.php
+++ web/articles/media.php
@@ -3,75 +3,74 @@
         <h1 style="width: 100%; text-align: center; margin-bottom: 20px"><?php __('page.media.headline'); ?></h1>
     </div>
     <div class="content" id="">
-        <div style="width: 500px; text-align: center; padding-left: calc(50% - 250px); line-height: 28px; margin-top: 80px"
-        ">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy Rethink Web Testing eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-        et accusam et justo duo dolores et ea rebum.
+        <div style="width: 500px; text-align: center; padding-left: calc(50% - 250px); line-height: 28px; margin-top: 80px">
+            Journalisten, Blogger, Podcaster - hier finden Medienschaffende aller Gattungen relevante Informationen über Leankoala und seine Gründer. Bei Interviewanfragen oder zu weiteren Materialien
+            wenden Sie sich bitte an den unten aufgeführten Pressekontakt.
+            <br><br>
+            <strong>Pressekontakt</strong><br>
+            Isabelle Ewald<br>
+            <a href="mailto:isabelle.ewald@leankoala.com">isabelle.ewald@leankoala.com</a>
+        </div>
 
-        <br><br>
-        <strong>Pressekontakt</strong><br>
-        Isabelle Ewald<br>
-        <a href="mailto:isabelle.ewald@leankoala.com">isabelle.ewald@leankoala.com</a>
+        <div style="width: 400px; text-align: center; padding-left: calc(50% - 200px); line-height: 28px; margin-top: 80px">
+            <div style="float: left; text-align: center; width: 150px">
+                <img src="/images/media/Download.png" style="margin-bottom: 20px"><br>
+                Unternehmensprofil
+            </div>
+            <div style="float: right; text-align: center; width: 150px">
+                <img src="/images/media/Download.png" style="margin-bottom: 20px"><br>
+                Pressemappe
+            </div>
+        </div>
+
+        <div style="clear: both"></div>
+
+        <div style="width: 800px; text-align: center; padding-left: calc(50% - 400px); line-height: 28px; margin-top: 150px">
+            <div style="float: left; margin-right: 80px; width: 410px; text-align: center">
+                <img src="/images/media/founders.png" style="margin-right: 20px"><br>
+            </div>
+            <div style="text-align: left">
+                <h1 style="margin-bottom: 40px">Die Gründer</h1>
+                Ein Bild sagt mehr als tausend Worte? Finden wir auch! Deshalb haben wir uns für unsere Pressefotos besonders herausgeputzt. Nutzen Sie das hier verfügbare Bildmaterial gerne für Ihre
+                Berichterstattung.
+                <ul id="founderslist">
+                    <li><a href="/media/Leankoala_Team.jpg"><img src="/images/media/list.png" style="margin-right: 10px"> Teamfoto</a></li>
+                    <li><a href="/media/Leankoala_Stefanie_Langner.jpg"><img src="/images/media/list.png" style="margin-right: 10px"> Stefanie Langner</a></li>
+                    <li><a href="/media/Leankoala_Nils_Langner.jpg"><img src="/images/media/list.png" style="margin-right: 10px"> Nils Langner</li>
+                </ul>
+            </div>
+        </div>
+
+        <div style="clear: both"></div>
+
+        <div style="width: 800px; text-align: center; padding-left: calc(50% - 400px); line-height: 28px; margin-top: 150px">
+            <div style="float: left; margin-right: 80px">
+                <img src="/images/media/logos.png" style="margin-right: 20px"><br>
+            </div>
+            <div style="text-align: left">
+                <h1 style="margin-bottom: 40px">Logos</h1>
+                Wählen Sie aus verschiedenen Ausführungen und Dateiformaten.
+                <ul id="founderslist">
+                    <li><a href="/media/logo.png"><img src="/images/media/list.png" style="margin-right: 10px"> Logo (png)</a></li>
+                    <li><a href="/media/logo.eps"><img src="/images/media/list.png" style="margin-right: 10px"> Logo (eps)</a></li>
+                    <li><a href="/media/logo_claim.png"><img src="/images/media/list.png" style="margin-right: 10px"> Logo with Claim (png)</a></li>
+                    <li><a href="/media/logo_claim.eps"><img src="/images/media/list.png" style="margin-right: 10px"> Logo with Claim (eps)</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <style>
+            #founderslist {
+                list-style: disc;
+                margin-top: 20px;
+            }
+
+            #founderslist li {
+                display: block;
+                float: none;
+            }
+        </style>
+
+
     </div>
-
-    <div style="width: 400px; text-align: center; padding-left: calc(50% - 200px); line-height: 28px; margin-top: 80px">
-        <div style="float: left; text-align: center; width: 150px">
-            <img src="/images/media/Download.png" style="margin-bottom: 20px"><br>
-            Unternehmensprofil
-        </div>
-        <div style="float: right; text-align: center; width: 150px">
-            <img src="/images/media/Download.png" style="margin-bottom: 20px"><br>
-            Pressemappe
-        </div>
-    </div>
-
-    <div style="clear: both"></div>
-
-    <div style="width: 800px; text-align: center; padding-left: calc(50% - 400px); line-height: 28px; margin-top: 150px">
-        <div style="float: left; margin-right: 80px; width: 410px; text-align: center">
-            <img src="/images/media/founders.png" style="margin-right: 20px"><br>
-        </div>
-        <div style="text-align: left">
-            <h1 style="margin-bottom: 40px">Founders</h1>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy Rethink Web Testing eirmod tempor invidunt.
-            <ul id="founderslist">
-                <li><a href="/media/Leankoala_Team.jpg"><img src="/images/media/list.png" style="margin-right: 10px"> Teamfoto</a></li>
-                <li><a href="/media/Leankoala_Stefanie_Langner.jpg"><img src="/images/media/list.png" style="margin-right: 10px"> Stefanie Langner</a></li>
-                <li><a href="/media/Leankoala_Nils_Langner.jpg"><img src="/images/media/list.png" style="margin-right: 10px"> Nils Langner</li>
-            </ul>
-        </div>
-    </div>
-
-    <div style="clear: both"></div>
-
-    <div style="width: 800px; text-align: center; padding-left: calc(50% - 400px); line-height: 28px; margin-top: 150px">
-        <div style="float: left; margin-right: 80px">
-            <img src="/images/media/logos.png" style="margin-right: 20px"><br>
-        </div>
-        <div style="text-align: left">
-            <h1 style="margin-bottom: 40px">Logos</h1>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy Rethink Web Testing eirmod tempor invidunt.
-            <ul id="founderslist">
-                <li><a href="/media/logo.png"><img src="/images/media/list.png" style="margin-right: 10px"> Logo (png)</a></li>
-                <li><a href="/media/logo.eps"><img src="/images/media/list.png" style="margin-right: 10px"> Logo (eps)</a></li>
-                <li><a href="/media/logo_claim.png"><img src="/images/media/list.png" style="margin-right: 10px"> Logo with Claim (png)</a></li>
-                <li><a href="/media/logo_claim.eps"><img src="/images/media/list.png" style="margin-right: 10px"> Logo with Claim (eps)</a></li>
-            </ul>
-        </div>
-    </div>
-
-
-    <style>
-        #founderslist {
-            list-style: disc;
-            margin-top: 20px;
-        }
-
-        #founderslist li {
-            display: block;
-            float: none;
-        }
-    </style>
-
-
-</div>
